b25e486334a4acb1dd3ad0d68636c177c73370a8
IrisWangHan <hanhan0101087@gmail.com>
2025-02-15 18:33:22 +0800

    加入專案檔案。

---
+++ VocalSpace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VocalSpace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
