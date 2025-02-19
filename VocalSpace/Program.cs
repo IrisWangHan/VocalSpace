75e038964ee55a0d175db1c47046e5123bcfe636
kevin-link <ryuzu1772@gmail.com>
2025-02-19 23:53:27 +0800

    2025.2.19 新增個人資料頁面

--- VocalSpace/Program.cs
+++ VocalSpace/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=accountSettings}/{action=memberInformation}/{id?}");
 
 app.Run();
