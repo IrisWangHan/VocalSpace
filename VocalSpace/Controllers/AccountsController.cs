756972c56e74673a0fb5fe5cf2d9671c5478aee5
s2312352 <s2312352@gmail.com>
2025-02-20 16:28:57 +0800

    忘記密碼頁/reCAPTCHA/判斷人機驗證/新增廣告圖

--- VocalSpace/Controllers/AccountsController.cs
+++ VocalSpace/Controllers/AccountsController.cs
@@ -12,5 +12,9 @@ namespace VocalSpace.Controllers
         {
             return View();
         }
+        public IActionResult ForgetPasswordDone()
+        {
+            return View();
+        }
     }
 }
