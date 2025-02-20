5556aa777a4cbd9a0a53f5a62c4430459affa836
s2312352 <s2312352@gmail.com>
2025-02-20 13:30:09 +0800

    新增account資料夾並把原login內容切換過去、新增AccountLayout與ForgetPassword頁面設計中

---
+++ VocalSpace/Controllers/AccountsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VocalSpace.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+    }
+}
