09988c2dc03bf5b8d4089587aa24fcafcc5dd846
4osoxm9 <ilyynlvl@gmail.com>
2025-02-20 15:10:24 +0800

    新增蒐藏庫/我的喜歡頁面

---
+++ VocalSpace/Controllers/CollectionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VocalSpace.Controllers
+{
+    public class CollectionController : Controller
+    {
+        public IActionResult like()
+        {
+            return View();
+        }
+    }
+}
