d430c9d3b1d7a90a86b5cc9ae252d485fa76fdf4
4osoxm9 <ilyynlvl@gmail.com>
2025-03-05 15:38:17 +0800

    新增個人頁面內各分頁、PersonalLayout更新

---
+++ VocalSpace/Controllers/PersonalController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VocalSpace.Controllers
+{
+    public class PersonalController : Controller
+    {
+        public IActionResult mymusic()
+        {
+            return View();
+        }
+        public IActionResult myabout()
+        {
+            return View();
+        }
+        public IActionResult mylist()
+        {
+            return View();
+        }
+        public IActionResult mylike()
+        {
+            return View();
+        }
+
+    }
+}
