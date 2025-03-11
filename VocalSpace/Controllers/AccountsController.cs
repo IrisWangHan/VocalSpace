69d1479a06184bd6214e650a3af17bb01c9e53d4
WChi <s2312352@gmail.com>
2025-03-12 01:52:51 +0800

    第一版後端登入(目前可以使用一般登入、提示標語待更新、登出按鈕可清除session、program檔設定HttpOnly代表只允許伺服器存取，還未有SSO)、更新donate畫面的btn focus效果

--- VocalSpace/Controllers/AccountsController.cs
+++ VocalSpace/Controllers/AccountsController.cs
@@ -1,13 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VocalSpace.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+
 
 namespace VocalSpace.Controllers
 {
     public class AccountsController : Controller
     {
+
+        private readonly VocalSpaceDbContext _context;
+        public AccountsController(VocalSpaceDbContext context)
+        {
+            _context = context;
+        }
+
+        // 確保頁面刷新時拿到最新狀態
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string account, string password)
+        {
+            var user = await _context.Users.Where(u => u.Account == account).FirstOrDefaultAsync();
+
+            if( user != null && user.Password == password)
+            {
+                HttpContext.Session.SetString("UserAccount", account);
+                HttpContext.Session.SetString("IsLoggedIn", "true");
+                return RedirectToAction("Index", "Home");
+            }
+            TempData["Error"] = "帳號或密碼錯誤";
+            Console.WriteLine(TempData["Error"]);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            Console.WriteLine("已清空session");
+            return RedirectToAction("Index", "Home");
+        }
+
         public IActionResult ForgetPassword()
         {
             return View();
