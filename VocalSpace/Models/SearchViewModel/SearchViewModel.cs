f4c2d9c9f33862fb21aa29389691df95a9663deb
kevin-link <ryuzu1772@gmail.com>
2025-03-13 00:19:49 +0800

    3/12 更新SearchViewModel

---
+++ VocalSpace/Models/SearchViewModel/SearchViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace VocalSpace.Models.SearchViewModel
+{
+    public class SearchViewModel
+    {
+        public List<SongInfoDTO>? Songs { get; set; }
+        public List<ArtistDTO>? Artists { get; set; } 
+        public List<PlaylistDTO>? Playlists { get; set; } 
+    }
+}
---
+++ VocalSpace/Models/SearchViewModel/SongInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace VocalSpace.Models.SearchViewModel
+{
+    public class SongInfoDTO
+    {
+        public string? SongPath { get; set; }
+
+        public string? CoverPhotoPath { get; set; }
+
+        public string? SongName { get; set; }
+        public string? UserName { get; set; }
+
+        public int? LikeCount { get; set; }
+    }
+}
