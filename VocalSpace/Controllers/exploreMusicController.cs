a99d3414879f6e4c0cdaf05aba76dfee8ca50587
kevin-link <ryuzu1772@gmail.com>
2025-03-14 17:12:32 +0800

    3/14 更新發現音樂後端

--- VocalSpace/Controllers/exploreMusicController.cs
+++ VocalSpace/Controllers/exploreMusicController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using VocalSpace.Models;
 using VocalSpace.Models.ViewModel.Search;
 
@@ -7,56 +8,87 @@ namespace VocalSpace.Controllers
     public class exploreMusicController : Controller
     {
         private readonly VocalSpaceDbContext _context;
-        public static List<SongInfoDTO> Allsongs;
-
+        //  type 代表歌曲類別
+        private static int type;
+        //  ExploreResult : 歌曲資訊 (歌名，歌手名，圖，喜歡數)
+        private static List<SongInfoDTO>? ExploreResult;
         public exploreMusicController(VocalSpaceDbContext context)
         {
             _context = context;
         }
+        //   歌曲資訊 LINQ 結果
+        private IQueryable<SongInfoDTO> ExploreSong( )
+        {
+            IQueryable<SongInfoDTO> a = from song in _context.Songs
+                                                         join user in _context.Users
+                                                         on song.Artist equals user.UserId
+                                                         join category in _context.SongCategories
+                                                         on song.SongCategoryId equals category.SongCategoryId
+                                                         select new SongInfoDTO { CoverPhotoPath = song.CoverPhotoPath, SongName = song.SongName, UserName = user.UserName, LikeCount = song.LikeCount, SongCategoryId = category.SongCategoryId };
+            return a;
+        }
         public IActionResult ExploreMusicAll()
         {
-            var a = from song in _context.Songs
-                    join user in _context.Users
-                    on song.Artist equals user.UserId
-                    select new SongInfoDTO { CoverPhotoPath = song.CoverPhotoPath, SongName = song.SongName, UserName = user.UserName, LikeCount = song.LikeCount };
-            Allsongs = a.ToList();
-            return View(Allsongs);
+            type = 0;
+   
+            ExploreResult = ExploreSong().ToList();
+            return View(ExploreResult);
         }
         public IActionResult ExploreMusicRock()
         {
-            return View();
+            type = 1;
+            ExploreResult = ExploreSong().Where(song => song.SongCategoryId == type).ToList();
+            return View( ExploreResult );
         }
         public IActionResult ExploreMusicFolk()
         {
-            return View();
+            type = 2;   //  Folk type == 2
+
+            ExploreResult = ExploreSong().Where(song => song.SongCategoryId == type).ToList();
+            return View(ExploreResult);
         }
         public IActionResult ExploreMusicHiphop()
         {
-            return View();
+            type = 3;   //  Hiphop type == 3
+
+            ExploreResult = ExploreSong().Where(song => song.SongCategoryId == type).ToList();
+            return View(ExploreResult);
         }
         public IActionResult ExploreMusicCitypop()
         {
-            return View();
+            type = 4;  //  Citypop type == 4
+
+            ExploreResult = ExploreSong().Where(song => song.SongCategoryId == type).ToList();
+            return View(ExploreResult);
         }
         public IActionResult ExploreMusicEDM()
         {
-            return View();
+            type = 5;  //  Edm type == 5
+
+            ExploreResult = ExploreSong().Where(song => song.SongCategoryId == type).ToList();
+            return View(ExploreResult);
         }
         public IActionResult ExploreMusicExplore()
         {
-            return View();
+            type = 6;   //  Edm type == 6
+
+            ExploreResult = ExploreSong().Where(song => song.SongCategoryId == type).ToList();
+            return View(ExploreResult);
         }
         [HttpGet]
         public IActionResult RankType()
         {
+
             switch (Request.Query["rt"])
             {
                 case "mostlike":
-
-                    return PartialView();             
+                    //  1. type == 0 ，代表全部歌曲，2. type != 0，篩選同類別歌曲(SongCategoryId == type)，2種情況之後依喜歡數排序(由大到小)
+                    ExploreResult = ( type == 0 ) ? ExploreSong().OrderByDescending(song => song.LikeCount).ToList() : ExploreSong().Where(song => song.SongCategoryId == type).OrderByDescending(song => song.LikeCount).ToList();
+                    break;
                 default:
-                    return PartialView("_partialViewSong");
+                    break;
             }
+            return PartialView("_partialViewSong", ExploreResult);
 
         }
         [HttpGet]
