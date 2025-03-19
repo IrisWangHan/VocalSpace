e2dd8eda335954899eb35fe0e35e87897d72ff50
iriswanghan <hanhan0101087@gmail.com>
2025-03-20 02:48:22 +0800

    修正  無法顯示上傳的歌曲到畫面上 因為畫面沒有更新:重新調整HTML結構 因為只是內容跑掉

--- VocalSpace/Models/ViewModel/Selection/SelectionFormViewModel.cs
+++ VocalSpace/Models/ViewModel/Selection/SelectionFormViewModel.cs
@@ -13,9 +13,12 @@ namespace VocalSpace.Models.ViewModel.Selection
         [JsonProperty("avatarPath")]
         public string? AvatarPath { get; set; }
 
+        [JsonProperty("applyStatusPath")]
+        public string? ApplyStatusPath { get; set; }
         [JsonProperty("file")]
         public IFormFile? File { get; set; }
-
+        [JsonProperty("applyStatusText")]
+        public string? ApplyStatusText { get; set; }
 
         [JsonProperty("applyStatus")]
         public int? ApplyStatus { get; set; }
