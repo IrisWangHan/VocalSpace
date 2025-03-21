7ad0f84f8b58d2f1b7ad3b9f6266fa3cac5f9832
iriswanghan <hanhan0101087@gmail.com>
2025-03-22 00:10:56 +0800

    修正訊息回傳內容(送出表單) 尚未串email

--- VocalSpace/Models/ViewModel/Selection/SelectionFormViewModel.cs
+++ VocalSpace/Models/ViewModel/Selection/SelectionFormViewModel.cs
@@ -4,6 +4,8 @@ namespace VocalSpace.Models.ViewModel.Selection
 {
     public class SelectionFormViewModel : SelectionListViewModel
     {
+        [JsonProperty("userId")]
+        public long? UserId { get; set; }
         [JsonProperty("userName")]
         public string? UserName { get; set; }
 
@@ -25,4 +27,24 @@ namespace VocalSpace.Models.ViewModel.Selection
         public List<SelectionSongs> ApplySongs { get; set; } = new List<SelectionSongs>();
 
     }
+    public class ApplicationRequest
+    {
+        [JsonProperty("selectionId")]
+        public long SelectionId { get; set; }
+        [JsonProperty("userName")]
+        public required string UserName { get; set; }
+        [JsonProperty("email")]
+        public required string Email { get; set; }
+        [JsonProperty("songs")]
+        public required List<Song> Songs { get; set; }
+    }
+
+    public class Song
+    {
+        [JsonProperty("songId")]
+        public int SongId { get; set; }
+        [JsonProperty("songName")]
+        public required string SongName { get; set; }
+    }
+
 }
